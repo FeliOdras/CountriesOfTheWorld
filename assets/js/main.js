648737afb7eb5a16261b1f002d441b53489dd705
FeliOdras <ariane.feldmann@odras.de>
2019-03-14 14:21:55 +0100

    Extended Output

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,19 +27,55 @@ class ShowCountryInfo {
         return countryList.filter(country => country.isSearchMatch == true);
     }
 
+    showlistOf(list, property) {
+        return list.map(
+            item => item[property]
+        ).join(', ')
+    }
+
+    countLanguages(languages) {
+        return (Object.keys(languages).length < 2 ?
+            'Language' :
+            'Languages')
+    }
+
+    countCurrencies(currencies) {
+        return (Object.keys(currencies).length < 2 ?
+            'Currency' :
+            'Currencies')
+    }
+
     showResults() {
         let resultList = this.searchByCountryName();
         console.log(resultList)
-        if (resultList.length < 1) {
-            return `There is no country name matching your search`
-        } else {
-            return resultList
-                .map(country => {
-                    return `
-                   <p> Name: ${country.name}</p>
+        return (resultList.length < 1 ?
+            `There is no country name matching your search` :
+            resultList
+            .map(country => {
+                return `
+                    <div class="singleCountry">
+                    <h3>
+                    ${country.name} (${country.alpha3Code})<br>
+                    ${country.regionalBlocs.length > 0 ? `<small>${this.showlistOf(country.regionalBlocs, 'name')} (${this.showlistOf(country.regionalBlocs, 'acronym')})</small>` : ``}
+                    </h3>
+                    <img src="${country.flag}" alt="${country.name}" width="150">
+                    <ul> 
+                        <li>Capital: ${country.capital}  </li>
+                        <li>Native Name: ${country.nativeName}</li>
+                        <li>${this.countLanguages(country.languages)}: ${this.showlistOf(country.languages, 'name')}</li>
+                        <li>Population: ${country.population}</li>
+                        <li>${this.countCurrencies(country.currencies)}: ${this.showlistOf(country.currencies, 'name')} (${this.showlistOf(country.currencies, 'symbol')})</li>
+                    </ul>
+                    <ul>
+                       
+                        <li>Calling Code: +${country.callingCodes}</li>
+                        <li>Region: ${country.region}</li>
+                        <li>Subrgion: ${country.subregion}</li>
+                        <li>TDL: ${country.topLevelDomain}
+                    </ul>
+                   </div>
                 `
-                }).join('');
-        }
+            }).join(''));
     }
 
     addEventListeners() {
