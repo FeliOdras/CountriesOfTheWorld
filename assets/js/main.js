83c6e916cee91742a54ae696d36e4bee73697f04
FeliOdras <ariane.feldmann@odras.de>
2019-03-14 12:12:14 +0100

    Added search method

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,23 @@ class ShowCountryInfo {
             .then(response => response.json())
             .then(countries => {
                 this.countries = countries
+                this.searchByCountryName()
             })
+    }
+
+    searchByCountryName() {
+        let countryList = this.countries;
+        countryList.forEach(
+            country => {
+                let searchValue = document.querySelector('.countrySearch').value.toLowerCase();
+                let countryName = country.name.toLowerCase();
+                let countryMatch = countryName.match(searchValue);
+                let searchMatch = ``;
+                countryMatch != null ? searchMatch = true : searchMatch = false;
+                country.isSearchMatch = searchMatch;
+            }
+        )
+        return countryList.filter(country => country.isSearchMatch == true);
     }
 }
 
