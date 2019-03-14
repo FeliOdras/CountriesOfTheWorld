0163e1157f0f8c9ce3697de52227744c6f1c0f88
FeliOdras <ariane.feldmann@odras.de>
2019-03-14 11:19:27 +0100

    Added fetch and Loop to ShowCountryInfo class

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,15 @@ class ShowCountryInfo {
     constructor() {
         this.init()
     }
+
+    init() {
+        fetch('https://restcountries.eu/rest/v2/all')
+            .then(response => response.json())
+            .then(countries => this.doLoop(countries))
+    }
+
+    doLoop(countries) {
+        countries.forEach(country => console.log(country))
+    }
 }
+const newCountryInfo = new ShowCountryInfo();
